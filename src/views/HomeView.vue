28d75b6cfcbc8946800e271d162eb35681f53d41
smith-maina7 <smithmaina7@gmail.com>
2025-06-13 10:58:17 +0300

    cleaned the project and set up the hero section

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <main>
+    <section
+      id="hero"
+      class="min-h-screen bg-gradient-to-r from-[#001C5E] via-[#084CA0] to-[#084698]"
+    ></section>
+  </main>
+</template>
